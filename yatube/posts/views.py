95dd24d6c7d0923f35e1b18a96efb5711292445d
tawaluk <tawaluk@yandex.ru>
2023-02-12 12:36:08 +0300

    шаблоны и группы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница') # Возвращает на экран надпись
+    template = 'posts/index.html' # Путь до html приложения!
+    return render(request, template) # Возвращает на экран надпись
 
 def group_posts(request, name_groop):
     return HttpResponse(f'Группа {name_groop}') # Вернет группа и слаг на экран
