bbad98e70a9e5bac3c0c98ecce0283f8d4e0a6f3
tawaluk <tawaluk@yandex.ru>
2023-02-12 13:55:25 +0300

    Задание В проекте Yatube удалите из шаблонов текст, описываюпередаваться в шаблон из view-функций через словарь context.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'posts/index.html' # Путь до html приложения!
-    return render(request, template) # Возвращает на экран надпись
+    title = 'Это главная страница проекта Yatube'
+    context_title = {
+        'title': title,
+    }
+    return render(request, template, context_title)
 
 def group_posts(request, name_groop):
-    return HttpResponse(f'Группа {name_groop}') # Вернет группа и слаг на экран
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context_title = {
+        'title': title,
+    }
+
+    return render(request, template, context_title)
