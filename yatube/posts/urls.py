315d8073502d8497fa02d956be8548c08cece601
tawaluk <tawaluk@yandex.ru>
2023-02-12 22:35:01 +0300

    добавил ссылку по логотипу на главную страницу

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:name_groop>/', views.group_posts)
+    path('', views.index, name='index'),
+    path('group/<slug:name_groop>/', views.group_posts, name='group_posts')
 ] 
