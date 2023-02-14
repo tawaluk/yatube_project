d6f60d809d45235fb2ce19266d5acdbd2a534893
tawaluk <tawaluk@yandex.ru>
2023-02-14 15:39:35 +0300

    прикрутил новую бд,обновил суперюзера

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,14 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    # Добавим в начало столбец pk
+    list_display = ('pk', 'text', 'pub_date', 'author') 
+    search_fields = ('text',) 
+    list_filter = ('pub_date',)  
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Post, PostAdmin) 
