da993bf2678e31b0f36cca0aef5c3e640b95f9e6
tawaluk <tawaluk@yandex.ru>
2023-02-14 21:37:28 +0300

    ОНО ЖИВОЕ

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:name_groop>/', views.group_posts, name='group_posts')
+    path('group/<slug:slug>/', views.group_posts, name='group_posts')
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from . models import Post, Group
 
 # Create your views here.
 def index(request):
     template = 'posts/index.html' # Путь до html приложения!
-    title = 'Это главная страница проекта Yatube'
+    title = 'Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context_title = {
         'title': title,
+        'posts': posts
     }
     return render(request, template, context_title)
 
-def group_posts(request, name_groop):
+
+def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
-    context_title = {
-        'title': title,
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
     }
-
-    return render(request, template, context_title)
+    return render(request, template, context) 
